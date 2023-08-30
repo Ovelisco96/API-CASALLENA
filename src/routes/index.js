9bc7a62b02304d1a07b4a9b414f68583f8231220
Ovelisco96 <123895378+Ovelisco96@users.noreply.github.com>
2023-08-29 21:01:21 -0500

    feat: html welcome

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import usersRouter from './users.js'
 import authRouter from './auth.router.js'
+import html from '../utils/html/welcome.js'
 
 const mainRouter = Router()
 
 mainRouter.use('/users', usersRouter)
 mainRouter.use('/auth', authRouter)
+mainRouter.use('/', (req, res) => res.type('html').send(html))
 
 export default mainRouter
